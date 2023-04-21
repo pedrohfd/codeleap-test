37feee8417d825fec479d32317655668523e92d6
Pedro Henrique <pedro.dedeus@luby.software>
2023-04-20 21:46:28 -0300

    refactor: improving semantic of splash file

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { AnimatedView, Container } from './styles'
+import React, { useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { StackScreenNavigationProps } from '../../types/StackNavigatorParamList'
 import CImage from '../../assets/c.svg'
 import OImage from '../../assets/o.svg'
 import DImage from '../../assets/d.svg'
@@ -8,79 +8,42 @@ import EImage from '../../assets/e.svg'
 import LImage from '../../assets/l.svg'
 import AImage from '../../assets/a.svg'
 import PImage from '../../assets/p.svg'
-import { StackScreenNavigationProps } from '../../types/StackNavigatorParamList'
+import { Container, AnimatedView } from './styles'
+
+const DELAY_STEP = 100
 
 export const Splash = () => {
   const navigation = useNavigation<StackScreenNavigationProps>()
-  setTimeout(() => {
-    navigation.navigate('SignUp')
-  }, 2000)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigation.navigate('SignUp')
+    }, 2000)
+
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  const renderAnimatedView = (image: React.ReactNode, delay: number) => (
+    <AnimatedView
+      from={{ transform: [{ translateX: 500 }] }}
+      animate={{ transform: [{ translateX: 0 }] }}
+      transition={{ type: 'spring', damping: 16 }}
+      delay={delay}
+    >
+      {image}
+    </AnimatedView>
+  )
 
   return (
     <Container>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-      >
-        <CImage width={40} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={100}
-      >
-        <OImage width={40} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={200}
-      >
-        <DImage width={40} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={300}
-      >
-        <EImage width={40} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={400}
-      >
-        <LImage width={14} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={500}
-      >
-        <EImage width={40} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={600}
-      >
-        <AImage width={40} />
-      </AnimatedView>
-      <AnimatedView
-        from={{ transform: [{ translateX: 500 }] }}
-        animate={{ transform: [{ translateX: 0 }] }}
-        transition={{ type: 'spring', damping: 16 }}
-        delay={700}
-      >
-        <PImage width={40} />
-      </AnimatedView>
+      {renderAnimatedView(<CImage width={40} />, DELAY_STEP * 0)}
+      {renderAnimatedView(<OImage width={40} />, DELAY_STEP * 1)}
+      {renderAnimatedView(<DImage width={40} />, DELAY_STEP * 2)}
+      {renderAnimatedView(<EImage width={40} />, DELAY_STEP * 3)}
+      {renderAnimatedView(<LImage width={14} />, DELAY_STEP * 4)}
+      {renderAnimatedView(<EImage width={40} />, DELAY_STEP * 5)}
+      {renderAnimatedView(<AImage width={40} />, DELAY_STEP * 6)}
+      {renderAnimatedView(<PImage width={40} />, DELAY_STEP * 7)}
     </Container>
   )
 }
