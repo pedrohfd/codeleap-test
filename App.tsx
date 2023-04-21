a44c2d5d794478ceb5f7d82d2acc2f30701f5660
Pedro Henrique <pedro.dedeus@luby.software>
2023-04-21 09:43:13 -0300

    feat: setting up fonts configuration

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
 import { AppRoutes } from './src/routes'
 import 'react-native-gesture-handler'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return <AppRoutes />
 }
